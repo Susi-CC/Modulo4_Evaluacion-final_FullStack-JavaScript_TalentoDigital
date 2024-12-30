ed240248d8615569f9849adb6aaf2962329735a7
Susana <176187436+Susi-CC@users.noreply.github.com>
2024-12-30 20:05:47 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
-// Llamar del html
 let parte1 = document.getElementById('range-1');
 let parte2 = document.getElementById('range-2');
 let parte3 = document.getElementById('range-3');
 let espacioPersonaje1 = document.getElementById('character-display1');
 let espacioPersonaje2 = document.getElementById('character-display2');
 let espacioPersonaje3 = document.getElementById('character-display3');
-
-
-//Personaje
 
 class Personajes {
     constructor(nombre,altura,peso) {
@@ -17,7 +13,6 @@ class Personajes {
         this.peso= peso;
     }
 
-    //Para llamar a la API
     static async crear(i) {
         let response = await fetch(`https://swapi.dev/api/people/${i}/`);
         let personaje = await response.json();
@@ -27,7 +22,6 @@ class Personajes {
 }
 
 
-// Fecth para sacar de la API y async await para promesa
 async function fetchPersonajes(inicio, fin) {
     var espacioPersonaje;
     if(fin <=6){
@@ -42,30 +36,24 @@ async function fetchPersonajes(inicio, fin) {
     }
 
 
-    espacioPersonaje.innerHTML = ''; // Que borre las cosas primero (reescribe vacío)
-    //El ciclo for
+    espacioPersonaje.innerHTML = ''; 
     for (let i = inicio; i < fin; i++) {
 
         let PersonajeNuevo = await Personajes.crear(i);
 
-        // Crear espacio para ficha de personaje
         let cartaPersonaje = document.createElement('div');
 
-        //Agregar la clase para ponerle colores al espacio y que se vea como tarjeta
         cartaPersonaje.classList.add('character-card');
 
-        //Agregar el texto a la tarjetita
         cartaPersonaje.innerHTML = `
             <h3>${PersonajeNuevo.nombre}</h3>
             <p>Altura: ${PersonajeNuevo.altura} cm</p>
             <p>Peso: ${PersonajeNuevo.peso} kg</p>
         `;
-        //Agregar tarjetas sin reescribir
         espacioPersonaje.appendChild(cartaPersonaje);
     }
 }
 
-// Hacer que "corra" la función
 parte1.addEventListener('mouseover', function() {
     fetchPersonajes(1, 6);
 });
@@ -76,7 +64,7 @@ parte3.addEventListener('mouseover', function() {
     fetchPersonajes(12, 17);
 });
 
-//Para saltarme el 17 que está dañado y poner el 18
+//Para saltarme el 17 (está dañado) y poner el 18
 parte3.addEventListener('mouseover', function() {
     fetchPersonajes(18, 19);
 });
